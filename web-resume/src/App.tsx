ea2fd7768e37a0fa907a64b92176f208d4eb063a
Hoan Lam <lamho@mail.gvsu.edu>
2025-04-29 10:58:59 -0400

    Center header and paragraph

--- web-resume/src/App.tsx
+++ web-resume/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './index.css'
 
 function App() {
   return (
     <>
-      <h1>Hoan Lam</h1>
-      <p>I am a GVSU student, and I like to program when I'm at work. Otherwise, I don't like working.</p>
+      <h2>Hello</h2>
+      <h1 className="text-center">Hoan Lam</h1>
+      <p className="text-center">I am a GVSU student, and I like to program when I'm at work. Otherwise, I don't like working.</p>
     </>
   )
 }
