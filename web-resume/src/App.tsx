efb0f5785d91bace9854fd1ce25fa942518ccc19
Hoan Lam <lamho@mail.gvsu.edu>
2025-04-29 14:15:43 -0400

    Add navigation

--- web-resume/src/App.tsx
+++ web-resume/src/App.tsx
@@ -1,13 +1,48 @@
 import './index.css'
+import Home from "./Home";
+import AboutMe from './AboutMe';
+import Projects from './projects';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <h2>Hello</h2>
-      <h1 className="text-center">Hoan Lam</h1>
-      <p className="text-center">I am a GVSU student, and I like to program when I'm at work. Otherwise, I don't like working.</p>
+      <BrowserRouter>
+        <header className="bg-gray-800 text-white p-4 flex justify-around">
+          <NavLink 
+            to="/" 
+            className={({ isActive }) => 
+            `px-4 py-2 rounded ${isActive ? 'bg-blue-500' : 'hover:bg-gray-700'}`
+            }
+          >
+            HOME
+          </NavLink>
+          <NavLink 
+            to="/about-me" 
+            className={({ isActive }) => 
+            `px-4 py-2 rounded ${isActive ? 'bg-blue-500' : 'hover:bg-gray-700'}`
+            }
+          >
+            About Me
+          </NavLink>
+          <NavLink 
+            to="/projects" 
+            className={({ isActive }) => 
+            `px-4 py-2 rounded ${isActive ? 'bg-blue-500' : 'hover:bg-gray-700'}`
+            }
+          >
+            Projects
+          </NavLink>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ web-resume/src/Home.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+
+function Home() {
+  return (
+    <>
+      <h1> Home Page </h1>
+      <h1 className="text-center">Hoan Lam</h1>
+      <p className="text-center">I am a GVSU student, and I like to program when I'm at work. Otherwise, I don't like working.</p>
+    </>
+  );
+}
+
+export default Home;
