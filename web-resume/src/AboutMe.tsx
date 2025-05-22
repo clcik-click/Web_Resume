7701bf8e029e57674cc73617977391e9ce25aabe
Hoan Lam <lamho@mail.gvsu.edu>
2025-05-22 09:17:10 -0400

    Modify About me page

--- web-resume/src/AboutMe.tsx
+++ web-resume/src/AboutMe.tsx
@@ -5,26 +5,141 @@ function AboutMe() {
         <div className="h-full overflow-y-auto p-6 bg-gray-100 rounded-lg shadow-lg space-y-6">
           <h1 className="text-2xl font-bold mb-4 text-center">😚 About Me <h1 className="wave">👋</h1></h1>
         </div>
-        <div className="mt-12 pr-4 grid grid-cols-3 gap-4 w-full">
+        <div className="mt-12 pr-4 grid grid-cols-2 gap-4 w-full">
           {/* left box */}
-          <div className = "relative mx-auto block w-96 overflow-hidden rounded-lg bg-slate-200 shadow-xl shadow-slate-200 ">
-            <img src="Chicken_Fried.jpeg" />
-          </div>
+            <div className="relative mx-auto flex flex-col justify-center w-96 overflow-hidden">
+              <img className="rounded-lg" src="Work.jpeg" />
+            </div>
 
           {/* right box - slightly offset*/}
-            <div className="p-6 col-span-2 bg-white rounded-lg shadow-md space-y-4">
-              <h2 className="text-xl font-semibold text-gray-800">Hoan Lam</h2>
-              <p className="text-gray-600 leading-relaxed">
-                Donec ultrices non erat dignissim luctus. Etiam ut urna quis sem sodales congue. <br />
-                Integer vel enim lacinia, mollis nisl vel, interdum lacus. In quis aliquam urna. <br />
-                Etiam ut pretium est, mollis auctor augue. <br />
-                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. <br />
-                In hac habitasse platea dictumst. Pellentesque aliquam sapien ac massa tristique, eget vulputate diam gravida. <br />
-                Suspendisse interdum sapien ac diam rhoncus lobortis. <br />
-                Maecenas felis leo, tincidunt condimentum placerat non, accumsan a erat. <br />
-                Donec ut auctor diam, id pretium massa.
-              </p>
+          {/* Timeline */}
+          <div>
+            {/* Item */}
+            <div className="group relative flex gap-x-5">
+              {/* Icon */}
+              <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+                <div className="relative z-10 size-6 flex justify-center items-center">
+                  <img
+                  src="/icons/GVSU_logo.svg"
+                  className="shrink-0 w-6 h-6 text-black"
+                  />
+                </div>
+              </div>
+              {/* End Icon */}
+
+              {/* Right Content */}
+              <div className="grow pb-8 group-last:pb-0">
+                <h3 className="mb-1 text-xs text-black">
+                  Grand Valley State University - Blue Nucleous - 2025 - Present
+                </h3>
+
+                <p className="font-semibold text-sm text-black">
+                  Software Developer
+                </p>
+
+                <p className="mt-1 text-sm text-black">
+                  The consultancy seeks to enrich computing education and empower both students and the community at large
+                </p>
+
+                <ul className="list-disc ms-6 mt-3 space-y-1.5">
+                  <li className="ps-1 text-sm text-black">
+                    Worked on sport scoreboard.
+                  </li>
+                  <li className="ps-1 text-sm text-black">
+                    Worked on Blue Nucleus website.
+                  </li>
+                </ul>
+
+                <div className="mt-3 w-2/3 mx-auto flex justify-center">
+                  {/* Card */}
+                  <a className="block border border-gray-200 rounded-lg hover:shadow-2xs focus:outline-hidden dark:border-neutral-700" href="https://www.gvsu.edu/bluenucleus/">
+                    <div className="relative flex items-center overflow-hidden">
+                      <img className="w-32 sm:w-48 h-full absolute inset-0 object-cover rounded-s-lg" src="GVSU.jpg"/>
+
+                      <div className="grow p-4 ms-32 sm:ms-48">
+                        <div className="min-h-24 flex flex-col justify-center">
+                          <h3 className="font-semibold text-sm text-black">
+                            Blue Nucleus
+                          </h3>
+                          <p className="mt-1 text-sm text-black">
+                            Where vision meets execution
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                  {/* End Card */}
+                </div>
+
+              </div>
+              {/* End Right Content */}
             </div>
+            {/* End Item */}
+
+            {/* Item */}
+            <div className="group relative flex gap-x-5">
+              {/* Icon */}
+              <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+                <div className="relative z-10 size-6 flex justify-center items-center">
+                  <img
+                    src="/icons/Toyota_logo.svg"
+                    className="shrink-0 w-6 h-6 text-black"
+                  />  
+                </div>
+              </div>
+              {/* End Icon */}
+
+              {/* Right Content */}
+              <div className="grow pb-8 group-last:pb-0">
+                <h3 className="mb-1 text-xs text-black">
+                  viastore, a TOYOTA Automated Logistics Company - 2019 - 2024
+                </h3>
+
+                <p className="font-semibold text-sm text-black">
+                  Controls Engineer Level 2
+                </p>
+
+                <ul className="list-disc ms-6 mt-3 space-y-1.5">
+                  <li className="ps-1 text-sm text-black">
+                    Clients Served: General Motors, Dollar General, Gordon Food Service, Tyson Foods, McMaster-Carr, Mar-Jac Poultry.
+                  </li>
+                  <li className="ps-1 text-sm text-black">
+                    Designed and implemented Human-Machine Interfaces (HMI), ensuring optimal user experience and functionality.
+                  </li>
+                  <li className="ps-1 text-sm text-black">
+                    Collaborated with multidisciplinary teams, including electricians, mechanics, controls/software engineers, and managers, to deliver seamless project execution.
+                  </li>
+                  <li className="ps-1 text-sm text-black">
+                    Led all electrical and programming site activities, including installation, commissioning, testing, and providing stand-by support.
+                  </li>
+                </ul>
+
+                <div className="mt-3 w-2/3 mx-auto flex justify-center">
+                  {/* Card */}
+                  <a className="block border border-gray-200 rounded-lg hover:shadow-2xs focus:outline-hidden dark:border-neutral-700" href="https://www.bastiansolutions.com/viastore-north-america/">
+                    <div className="relative flex items-center overflow-hidden">
+                      <img className="w-32 sm:w-48 h-full absolute inset-0 object-cover rounded-s-lg" src="viastore.jpg"/>
+
+                      <div className="grow p-4 ms-32 sm:ms-48">
+                        <div className="min-h-24 flex flex-col justify-center">
+                          <h3 className="font-semibold text-sm text-black">
+                            viastore
+                          </h3>
+                          <p className="mt-1 text-sm text-black">
+                            Garantee Success !!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                  {/* End Card */}
+                </div>
+              </div>
+              {/* End Right Content */}
+            </div>
+            
+          </div>
+          {/* End Timeline */}
         </div>
 
       </main>
--- web-resume/src/Projects.tsx
+++ web-resume/src/Projects.tsx
@@ -6,34 +6,93 @@ function Projects() {
           <h1 className="text-2xl font-bold mb-4 text-center">📒 Projects</h1>
         </div>
         
-        <div className="p-6">
-          <p className="text-gray-700">
-            Integer accumsan urna semper, viverra ex vitae, maximus lacus. Aliquam id luctus elit, vel lobortis odio. Sed cursus aliquet velit, ut elementum ipsum pharetra a. Nunc id sapien nibh. Ut mattis nulla nulla. Suspendisse ullamcorper, arcu vestibulum accumsan volutpat, libero mauris viverra elit, ultrices scelerisque magna tellus sed sapien. Nunc blandit magna vitae tellus tristique feugiat. Aenean tincidunt iaculis tincidunt. Quisque venenatis blandit sagittis. Morbi at ligula eu purus lobortis sollicitudin. Nulla nec ipsum sit amet sapien lobortis venenatis. Maecenas consequat dui at cursus suscipit. Aenean dapibus dapibus pretium.
-          </p>
-          <br />
-          <ul className="space-y-4">
-            <li className="p-4 bg-white rounded-lg shadow-md">
-              <h2 className="text-lg font-semibold">Project #1</h2>
-              <p className="text-gray-600">Suspendisse fermentum elementum ultricies. Sed dui dui, iaculis a aliquam et, imperdiet ut nulla. Praesent sagittis laoreet turpis, at imperdiet nunc accumsan in.</p>
-            </li>
-            <li className="p-4 bg-white rounded-lg shadow-md">
-              <h2 className="text-lg font-semibold">Project #2</h2>
-              <p className="text-gray-600">Quisque molestie risus nec ultricies dapibus. Aliquam commodo malesuada orci, id imperdiet nulla. Fusce fringilla, urna vel tristique malesuada, lectus ex ultrices diam, at euismod est dui sit amet dui.</p>
-            </li>
-            <li className="p-4 bg-white rounded-lg shadow-md">
-              <h2 className="text-lg font-semibold">Project #3</h2>
-              <p className="text-gray-600">Vivamus vestibulum leo ut ante aliquam, id vulputate orci eleifend. Proin tristique neque sit amet hendrerit fermentum.</p>
-            </li>
-            <li className="p-4 bg-white rounded-lg shadow-md">
-              <h2 className="text-lg font-semibold">Project #4</h2>
-              <p className="text-gray-600">Integer accumsan urna semper, viverra ex vitae, maximus lacus. Aliquam id luctus elit, vel lobortis odio. Sed cursus aliquet velit, ut elementum ipsum pharetra a. Nunc id sapien nibh. Ut mattis nulla nulla. Suspendisse ullamcorper, arcu vestibulum accumsan volutpat, libero mauris viverra elit, ultrices scelerisque magna tellus sed sapien.</p>
-            </li>
-            <li className="p-4 bg-white rounded-lg shadow-md">
-              <h2 className="text-lg font-semibold">Project #5</h2>
-              <p className="text-gray-600">Nunc blandit magna vitae tellus tristique feugiat. Aenean tincidunt iaculis tincidunt. Quisque venenatis blandit sagittis. Morbi at ligula eu purus lobortis sollicitudin. Nulla nec ipsum sit amet sapien lobortis venenatis. Maecenas consequat dui at cursus suscipit. Aenean dapibus dapibus pretium.</p>
-            </li>
-          </ul>
+        {/* Masonry Cards */}
+        <div className="max-w-6xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+          {/* Grid */}
+          <div className="grid sm:grid-cols-12 gap-6">
+            <div className="sm:self-end col-span-12 sm:col-span-7 md:col-span-8 lg:col-span-5 lg:col-start-3">
+              {/* Card */}
+              <a className="group relative block rounded-xl overflow-hidden focus:outline-hidden" href="#">
+                <div className="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden">
+                  <img className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover" src="https://images.unsplash.com/photo-1606868306217-dbf5046868d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Masonry Cards Image" />
+                </div>
+                <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+                  <div className="text-sm font-semibold text-gray-800 rounded-lg bg-white p-4 md:text-xl dark:bg-neutral-800 dark:text-neutral-200">
+                    Workplace personalities
+                  </div>
+                </div>
+              </a>
+              {/* End Card */}
+            </div>
+            {/* End Col */}
+
+            <div className="sm:self-end col-span-12 sm:col-span-5 md:col-span-4 lg:col-span-3">
+              {/* Card */}
+              <a className="group relative block rounded-xl overflow-hidden focus:outline-hidden" href="#">
+                <div className="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden">
+                  <img className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover" src="https://images.unsplash.com/photo-1605629921711-2f6b00c6bbf4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Masonry Cards Image" />
+                </div>
+                <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+                  <div className="text-sm font-semibold text-gray-800 rounded-lg bg-white p-4 md:text-xl dark:bg-neutral-800 dark:text-neutral-200">
+                    Women in engineering
+                  </div>
+                </div>
+              </a>
+              {/* End Card */}
+            </div>
+            {/* End Col */}
+
+            <div className="col-span-12 md:col-span-4">
+              {/* Card */}
+              <a className="group relative block rounded-xl overflow-hidden focus:outline-hidden" href="#">
+                <div className="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden">
+                  <img className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover" src="https://images.unsplash.com/photo-1606836576983-8b458e75221d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Masonry Cards Image" />
+                </div>
+                <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+                  <div className="text-sm font-semibold text-gray-800 rounded-lg bg-white p-4 md:text-xl dark:bg-neutral-800 dark:text-neutral-200">
+                    Pride 2021
+                  </div>
+                </div>
+              </a>
+              {/* End Card */}
+            </div>
+            {/* End Col */}
+
+            <div className="col-span-12 sm:col-span-6 md:col-span-4">
+              {/* Card */}
+              <a className="group relative block rounded-xl overflow-hidden focus:outline-hidden" href="#">
+                <div className="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden">
+                  <img className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover" src="https://images.unsplash.com/photo-1598929438701-ef29ab0bb61a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Masonry Cards Image" />
+                </div>
+                <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+                  <div className="text-sm font-semibold text-gray-800 rounded-lg bg-white p-4 md:text-xl dark:bg-neutral-800 dark:text-neutral-200">
+                    Data at Preline
+                  </div>
+                </div>
+              </a>
+              {/* End Card */}
+            </div>
+            {/* End Col */}
+
+            <div className="col-span-12 sm:col-span-6 md:col-span-4">
+              {/* Card */}
+              <a className="group relative block rounded-xl overflow-hidden focus:outline-hidden" href="#">
+                <div className="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden">
+                  <img className="group-hover:scale-105 group-focus:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover" src="https://images.unsplash.com/photo-1467043153537-a4fba2cd39ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80" alt="Masonry Cards Image" />
+                </div>
+                <div className="absolute bottom-0 start-0 end-0 p-2 sm:p-4">
+                  <div className="text-sm font-semibold text-gray-800 rounded-lg bg-white p-4 md:text-xl dark:bg-neutral-800 dark:text-neutral-200">
+                    Empowered management
+                  </div>
+                </div>
+              </a>
+              {/* End Card */}
+            </div>
+            {/* End Col */}
+          </div>
+          {/* End Grid */}
         </div>
+        {/* End Masonry Cards */}
 
       </main>
     </>
